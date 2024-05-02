a9806ea973e7fbda0b611e55554488d439363e59
Fibra12 <febrianbrhm@gmail.com>
2024-05-02 20:07:56 +0700

    pembuatan Menu

--- StudiiKasus/My Project/Application.Designer.vb
+++ StudiiKasus/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.StudiiKasus.frPeriodeLaporan
+            Me.MainForm = Global.StudiiKasus.frUtama
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- StudiiKasus/frUtama.vb
+++ StudiiKasus/frUtama.vb
@@ -7,4 +7,23 @@
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         ToolStripStatusLabel1.Text = TimeOfDay
     End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub PegawaiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PegawaiToolStripMenuItem.Click
+        Dim frPgw As New frPegawai
+        frPgw.Show()
+    End Sub
+
+    Private Sub PegawaiToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles PegawaiToolStripMenuItem1.Click
+        Dim frRpPgw As New frRpPegawai
+        frRpPgw.Show()
+    End Sub
+
+    Private Sub TransaksiToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles TransaksiToolStripMenuItem1.Click
+        Dim frTgl As New frPeriodeLaporan
+        frTgl.Show()
+    End Sub
 End Class
