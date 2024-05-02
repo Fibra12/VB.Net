e662aeb3a0790afa3ad365e2efb32a933782c472
Fibra12 <febrianbrhm@gmail.com>
2024-05-02 19:46:26 +0700

    Studi kasus laporan penjualan 1. Memastikan tabel2 sudah benar relasinya. 2. mendesain laporan 3. menampilkan laporan menggunakan form

--- StudiiKasus/My Project/Application.Designer.vb
+++ StudiiKasus/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.StudiiKasus.frPilihKelamin
+            Me.MainForm = Global.StudiiKasus.frPeriodeLaporan
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- StudiiKasus/frLaporan.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frLaporan
-
-End Class
